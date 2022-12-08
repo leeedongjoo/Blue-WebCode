c6c65005694c33494bf28319356d112d3e8cee6b
이동주 <leedjcap@gmail.com>
2022-12-08 11:42:22 +0900

    name 입력하면 바뀌기

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,18 @@
-const test = 10;
+const headerUserNameElement = document.querySelector('.user');
+const userNameElement = document.querySelector('.user-name');
+const localUserName = localStorage.getItem('userName');
 
-console.log(test);
+const setUserNameInnerHtml = (name) =>{
+  headerUserNameElement.innerHTML = `${name} <span>님</span>`;
+  userNameElement.innerHTML = `${name} <span>님</span>`;
+};
+if (localUserName) {
+  setUserNameInnerHtml(localUserName);
+}
+
+userNameElement.onclick = () =>{
+  const userName = prompt('이름을 입력해 주세요.');
+  localStorage.setItem('userName', userName);
+
+  setUserNameInnerHtml(userName);
+};
